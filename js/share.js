2abe6aaf3aeb4657b745cd2caa139bbbdcd19e27
tastytestlab <167013422+tastytestlab@users.noreply.github.com>
2024-04-16 00:30:19 +0900

    Add files via upload

--- js/share.js
+++ js/share.js
@@ -1,67 +1,37 @@
+const url = 'https://https://justest.shop/';
+
+function setShare() {
+    var mainImg = document.querySelector('#mainImage');
+    var mainAlt = mainImg.firstElementChild.alt;
+    const shareTitle = '연애유형 컬러 테스트';
+    const shareDes = infoList[mainAlt].name;
+    const shareImage = url + 'img/image-' + mainAlt + '.png';
+    const shareURL = url;
+}
+
 function kakaoShare() {
     Kakao.Share.sendDefault({
         objectType: 'feed',
         content: {
-            title: '오늘의 디저트',
-            description: '아메리카노, 빵, 케익',
+            title: shareTitle,
+            description: shareDes,
             imageUrl:
-                'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+                shareImage,
             link: {
-                mobileWebUrl: 'https://developers.kakao.com',
-                webUrl: 'https://developers.kakao.com',
+                mobileWebUrl: shareURL,
+                webUrl: shareURL,
             },
         },
-        itemContent: {
-            profileText: 'Kakao',
-            profileImageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
-            titleImageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
-            titleImageText: 'Cheese cake',
-            titleImageCategory: 'Cake',
-            items: [
-                {
-                    item: 'Cake1',
-                    itemOp: '1000원',
-                },
-                {
-                    item: 'Cake2',
-                    itemOp: '2000원',
-                },
-                {
-                    item: 'Cake3',
-                    itemOp: '3000원',
-                },
-                {
-                    item: 'Cake4',
-                    itemOp: '4000원',
-                },
-                {
-                    item: 'Cake5',
-                    itemOp: '5000원',
-                },
-            ],
-            sum: '총 결제금액',
-            sumOp: '15000원',
-        },
-        social: {
-            likeCount: 10,
-            commentCount: 20,
-            sharedCount: 30,
-        },
+
         buttons: [
             {
                 title: '웹으로 이동',
                 link: {
-                    mobileWebUrl: 'https://developers.kakao.com',
-                    webUrl: 'https://developers.kakao.com',
+                    mobileWebUrl: shareURL,
+                    webUrl: shareURL,
                 },
             },
-            {
-                title: '앱으로 이동',
-                link: {
-                    mobileWebUrl: 'https://developers.kakao.com',
-                    webUrl: 'https://developers.kakao.com',
-                },
-            },
+
         ],
     });
 }
