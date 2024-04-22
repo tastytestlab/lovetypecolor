7d71331b5726c7074122a0a89e0f134fa250a3f3
tastytestlab <167013422+tastytestlab@users.noreply.github.com>
2024-04-22 21:24:17 +0900

    Add files via upload

--- js/share.js
+++ js/share.js
@@ -1,46 +1,34 @@
-const url = 'https://justest.shop/';
+const url = 'https://justest.shop';
 
-function kakaoShare() {
-    Kakao.Share.createDefaultButton({
-        container: '#kakaotalk-sharing-btn',
-        objectType: 'feed',
-        content: {
-            title: '연애유형 컬러 테스트!',
-            description: '핫한 질문들로 알아보는 연애유형 컬러 테스트!',
-            imageUrl:
-                'img/share.png',
+function setShare() {
+    var resultImg = document.querySelector('#resultImg')
+    var resultAlt = resultImg.firstElementChild.alt;
+    const shareTitle = '연애유형 컬러 테스트 결과';
+    const shareDes = infoList[resultAlt].name;
+    const shareImage = url + 'img/image-' + resultAlt + '.png';
+    const shareURL = url + 'page.result-' + resultAlt + '.html';
+}
+
+Kakao.link.sendDefault({
+    objectType: 'feed',
+    content: {
+        title: shareTitle,
+        description: shareDes,
+        imageUrl:
+            shareImage,
+        link: {
+            mobileWebUrl: shareURL,
+            webUrl: shareURL,
+        },
+    },
+    buttons: [
+        {
+            title: '결과 확인하기',
             link: {
-                mobileWebUrl: 'https://justest.shop/',
-                webUrl: 'https://justest.shop/',
+                mobileWebUrl: shareURL,
+                webUrl: shareURL,
             },
         },
-        itemContent: {
-            profileText: 'Kakao',
-            profileImageUrl: 'https://justest.shop/',
-            titleImageUrl: 'img/share.png',
-            titleImageText: '',
-            titleImageCategory: '',
-        },
-        social: {
-            likeCount: 10,
-            commentCount: 20,
-            sharedCount: 30,
-        },
-        buttons: [
-            {
-                title: '웹으로 이동',
-                link: {
-                    mobileWebUrl: 'https://developers.kakao.com',
-                    webUrl: 'https://developers.kakao.com',
-                },
-            },
-            {
-                title: '앱으로 이동',
-                link: {
-                    mobileWebUrl: 'https://developers.kakao.com',
-                    webUrl: 'https://developers.kakao.com',
-                },
-            },
-        ],
-    });
-}    
+    ],
+})
+
